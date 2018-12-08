c64cb26a867eee5b965a873236191cf71ca84e0d
han8909227 <hbao2016@hotmail.com>
2018-12-08 13:41:29 -0800

    basic component structured

--- client/src/components/ListPlaces.js
+++ client/src/components/ListPlaces.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import './../styles/App.css';
+import { Divider } from 'semantic-ui-react'
+import MerchItem from 'MerchItem';
 
 class ListPlaces extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      merchesArray: [
+        {name:'Bob Shop', id: 1},
+        {name: 'John Store', id: 2}
+      ]
+    }
+
+
+    
+    const PlaceLits = () => {
+
+      merchesArray.map(merch => {
+        <MerchItem onCLick={} item={merch} id={merch.id}/>
+
+      })
+    }
+  }
+
+  componentDidMount(){
+    fetch()
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          Place
-        </header>
+
+
+
+      <div class="HaggleContainer">
+        {PlaceLits}
+      <Divider/>
       </div>
     );
   }
