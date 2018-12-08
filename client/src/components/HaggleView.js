93a520abd4ef2e8d86835bf956f284b7cc13eeef
yichiang <yi_chiang@Outlook.com>
2018-12-08 14:24:05 -0800

    delete discover calls from server

--- client/src/components/HaggleView.js
+++ client/src/components/HaggleView.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
 import './../styles/App.css';
-import { Divider } from 'semantic-ui-react'
-import DiscoverHttp from './../extension/discoverHttp'
+import { Divider, Button } from 'semantic-ui-react'
 class HaggleView extends Component {
 
-
+constructor(){
+  super();
+  this.state = {
+    propsedValues: 0
+  }
+  this.allButtons = this.getNumbers();
+}
 componentDidMount() {
-  var discoverHttp = new DiscoverHttp();
-  discoverHttp.getData('http://localhost:4200/getExchangeRate').then(res => console.log(res));
+
+  fetch('http://localhost:4200/getExchangeRate', {
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+          "Content-Type": "application/json; charset=utf-8",
+      },
+  })
+  .then(response => { console.log("response", response); response.json()})
 }
 
-
+getNumbers(){
+  var allButtons = [];
+   for (var i = 1; i < 10; i++) {
+     allButtons.push(i)
+   }
+   return allButtons;
+}
+OnPressChangeVal(value){
+  console.log(value)
+  this.setState({propsedValues: value})
+}
   render() {
     return (
       <div>
-      $10
-      <Divider/>
+        <div>${this.state.propsedValues}</div>
+        {this.allButtons.map(x => <Button key={x} onClick={() => this.OnPressChangeVal(x)}>{x}</Button>)}
+        <Divider/>
+        <div>
+      </div>
       </div>
     );
   }
