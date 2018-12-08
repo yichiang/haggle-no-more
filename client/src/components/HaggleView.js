bf8110a76e4e3c518cd01de2a0461374735a607d
yichiang <yi_chiang@Outlook.com>
2018-12-08 14:02:12 -0800

    add server

--- client/src/components/HaggleView.js
+++ client/src/components/HaggleView.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import './../styles/App.css';
 import { Divider } from 'semantic-ui-react'
-
+import DiscoverHttp from './../extension/discoverHttp'
 class HaggleView extends Component {
+
+
+componentDidMount() {
+  var discoverHttp = new DiscoverHttp();
+  discoverHttp.getData('http://localhost:4200/getExchangeRate').then(res => console.log(res));
+}
+
+
   render() {
     return (
       <div>
